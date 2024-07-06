ebd2e71268b2dfe0da6afd1d981479a085f34811
kousikdas2 <kousikdasbabu2@gmail.com>
2024-07-07 00:08:52 +0530

    styled components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Checkbox, Container, Dialog, DialogActions, DialogContent, DialogTitle, FormControlLabel, Grid, styled, TextField, Typography } from '@mui/material'
+import { Box, Button, Checkbox, Container, Dialog, DialogActions, DialogContent, DialogTitle, FormControlLabel, Grid, TextField, Typography } from '@mui/material'
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../../assets/images/Logo'
